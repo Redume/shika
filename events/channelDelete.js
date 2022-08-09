ff77f6b012cad682b45784eaa0af4aa74da8e1cd
Redume <Danilivasenko09@gmail.com>
2022-08-09 09:52:51 +0300

    Исправление ошибок

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,10 +1,10 @@
 const pool = require("../postgresql");
 module.exports = async(client, channel) => {
     if(channel.type !== 0) return;
-    const user = await pool.query("SELECT * FROM person WHERE guild_id = $1", [channel.guildId]);
+    const user = await pool.query("SELECT * FROM person WHERE channel_id = $1", [channel.id]);
     if(user.rows.length === 0) return;
     if(user.rows[0].channel_id === channel.id) {
-        await pool.query("UPDATE person SET channel_id = 'None', blog = false WHERE channel_id = $1", [channel.id]);
+        await pool.query("UPDATE person SET channel_id = 'None', blog = false, messages = 0 WHERE channel_id = $1", [channel.id]);
         await pool.query("UPDATE guild SET blogs_count = blogs_count - 1 WHERE guild_id = $1", [channel.guildId]);
     }
 }
