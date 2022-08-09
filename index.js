210b5504de354d620a9b6a0bd599baa8acb380ce
Redume <Danilivasenko09@gmail.com>
2022-08-09 09:52:26 +0300

    Добавление прототипов

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const { GatewayIntentBits, Client, Collection } = require('discord.js');
+const { GatewayIntentBits, Client, Collection, Guild, GuildMember } = require('discord.js');
 const fs = require("fs");
 const yaml = require("yaml");
+
+const pool = require("./postgresql");
+
 const client = new Client({allowedMentions: { parse: [], repliedUser: false },
         intents: [GatewayIntentBits.Guilds,
                 GatewayIntentBits.GuildMessages,
@@ -14,7 +17,31 @@ client.slashArray = [];
 
 const config = yaml.parse(fs.readFileSync('./config.yaml', 'utf8'))
 
-require("./function/logger")();
+require("./utils/logger")();
+
+Guild.prototype.getData = async function () {
+    const data = await pool.query("SELECT * FROM guild WHERE guild_id = $1", [this.id]);
+    if(!data) await pool.query("INSERT INTO guild (guild_id) VALUES ($1) RETURNING *", [this.id]);
+    return data.rows[0];
+};
+
+GuildMember.prototype.getData = async function () {
+    const data = await pool.query(
+        "SELECT * FROM person WHERE user_id = $1 AND guild_id = $2",
+        [
+            this.user.id,
+            this.guild.id
+        ]
+    );
+
+    if(!data) await pool.query("INSERT INTO person (user_id, guild_id) VALUES ($1, $2) RETURNING *",
+        [
+            this.user.id,
+            this.guild.id
+        ]
+    );
+    return data.rows[0];
+};
 
 ["commands", "slash", "events"].forEach(handler => {
     require(`./handler/${handler}`)(client);
